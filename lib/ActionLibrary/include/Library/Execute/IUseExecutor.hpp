4ab8d116ed2872fa44980e2ba6834e5e41d24ac5
shibbo <supershibbo12@gmail.com>
2024-10-10 18:45:29 -0400

    `LiveActorKit` header

--- lib/ActionLibrary/include/Library/Execute/IUseExecutor.hpp
+++ lib/ActionLibrary/include/Library/Execute/IUseExecutor.hpp
@@ -5,5 +5,7 @@ namespace al {
     public:
         virtual void execute() = 0;
         virtual void draw() const;
+        virtual const char* executorName() const = 0;
+        virtual void executeOnThread() = 0;
     };
 };
